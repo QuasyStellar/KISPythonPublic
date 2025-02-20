45999aa59e1c60ab06847b8db3a772280f15de39
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2025-02-20 10:22:15 +0300

    Update main.py

--- Practice/TASK3/main.py
+++ Practice/TASK3/main.py
@@ -1,3 +1,4 @@
+#Variant 1
 def main(m, n):
     itr1 = 0
     for j in range(1, n+1):
@@ -11,13 +12,28 @@ def main(m, n):
     return itr1
 
 
+#Variant 2
 def main(m, n):
     sum_j = sum(26 * j**2 - (j - 38 * j**2) ** 7 for j in range(1, n + 1))
     sum_c = sum(5 * c**3 for c in range(1, m + 1))
     return sum_j * m - sum_c * n
 
-
+#Variant 3
 def main(m, n):
     sum_j = sum(map(lambda j: 26 * j**2 - (j - 38 * j**2) ** 7, range(1, n + 1)))
     sum_c = sum(map(lambda c: 5 * c**3, range(1, m + 1)))
     return sum_j * m - sum_c * n
+
+#Variant 5
+def sum_j(n):
+    if n == 0:
+        return 0
+    return (26 * n**2 - (n - 38 * n**2) ** 7) + sum_j(n - 1)
+
+def sum_c(m):
+    if m == 0:
+        return 0
+    return 5 * m**3 + sum_c(m - 1)
+
+def main(m, n):
+    return sum_j(n) * m - sum_c(m) * n
