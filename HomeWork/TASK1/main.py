a2a005b86c93e10c66d88552e5cde18913202499
quasys <76686085+QuasyStellar@users.noreply.github.com>
2025-02-23 20:48:22 +0300

    All variants added

--- HomeWork/TASK1/main.py
+++ HomeWork/TASK1/main.py
@@ -6,5 +6,37 @@
 #     part2 = 14 * (88 * z**2 - 97 * y**3)**4
 #     return part1 + part2
 
-if __name__ == "__main__":
-    print(main())
+# Variant 2
+# import math
+#
+#
+# def calc_sqr(A):
+#     return math.sqrt(48) * abs(A)
+#
+#
+# def calc_rht(z, y):
+#     return 14 * (88 * z**2 - 97 * y**3) ** 4
+#
+#
+# def main(z, y) -> float:
+#     return calc_sqr((y**3) / 30 - z - 31) + calc_rht(z, y)
+
+# Variant 3
+# def main(z, y) -> float:
+#     sqr = (48 * ((y**3) / 30 - z - 31) ** 2) ** 0.5
+#     rht = 14 * (88 * z**2 - 97 * y**3) ** 4
+#     return sqr + rht
+
+# Variant 4
+from functools import reduce
+
+
+def add(x, y):
+    return x + y
+
+
+def main(z, y) -> float:
+    return reduce(
+        add,
+        [(48 * ((y**3 / 30 - z - 31) ** 2)) ** 0.5, 14 * (88 * z**2 - 97 * y**3) ** 4],
+    )
--- HomeWork/TASK2/main.py
+++ HomeWork/TASK2/main.py
@@ -10,5 +10,34 @@
 #         return 82 + y**2
 #     return 56 * y**6
 
+# Variant 2
+# def main(y):
+#     conditions = [
+#         (y < 147, (y**3 / 65) ** 5 - y**3),
+#         (147 <= y < 192, y**2 - 1 - 15 * (y**0.5) ** 3),
+#         (192 <= y < 214, 25 * y**6),
+#         (214 <= y < 259, 82 + y**2),
+#         (y >= 259, 56 * y**6),
+#     ]
+#     return next(result for condition, result in conditions if condition)
+
+# Variant 3
+# import bisect
+#
+#
+# def main(y):
+#     thresholds = [147, 192, 214, 259]
+#     functions = [
+#         lambda y: (y**3 / 65) ** 5 - y**3,
+#         lambda y: y**2 - 1 - 15 * (y**0.5) ** 3,
+#         lambda y: 25 * y**6,
+#         lambda y: 82 + y**2,
+#         lambda y: 56 * y**6,
+#     ]
+#
+#     idx = bisect.bisect_right(thresholds, y)
+#     return functions[idx](y)
+#
+
 if __name__ == "__main__":
     print(main(102))
