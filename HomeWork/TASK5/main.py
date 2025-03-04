a154952793fc6edbdca3b7d9242d81ef6ee9eb08
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2025-03-04 11:21:16 +0300

    Create main.py
    Variants 1-2

---
+++ HomeWork/TASK5/main.py
@@ -0,0 +1,13 @@
+#Varian 1
+def main(z, y):
+    n = len(y)
+    s = 0
+    for i in range(1, n + 1):
+        s += 21 * (y[n - i] + z[i // 3] ** 2)
+    return 62 * s
+
+#Variant 2
+def main(z, y):
+    n = len(y)
+    return 62 * sum(21 * (y[n - i] + z[i // 3] ** 2)
+                    for i in range(1, n + 1))
